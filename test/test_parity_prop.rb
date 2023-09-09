d2a4e8ad0a818e0342a5f5845c71509c62d4be86
rlogwood <richard.logwood@gmail.com>
2023-09-09 01:39:08 -0400

    add test

---
+++ test/test_parity_prop.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require_relative "../lib/parity_prop"
+
+class ParityPropTest < Minitest::Test
+  def test_split_by_parity
+    arr = [1,2,3,4,5,6,7,8,9,10]
+    assert_equal arr.partition(&:even?), arr.split_by_parity
+  end
+end
